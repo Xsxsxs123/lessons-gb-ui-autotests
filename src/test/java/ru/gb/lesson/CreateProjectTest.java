e7608e6169a24a6409d44fb8d0dc8c4c60165204
Xsxsxs123 <andr.ivanov1977@gmail.com>
2021-12-25 14:00:33 +0300

    Lesson3

---
+++ src/test/java/ru/gb/lesson/CreateProjectTest.java
@@ -0,0 +1,20 @@
+package ru.gb.lesson;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple CreateProject.
+ */
+public class CreateProjectTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue(true);
+    }
+}
